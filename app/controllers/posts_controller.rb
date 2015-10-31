3486b09e2052fa917ccd814c72694f42863ccf80
Leonenko Igor <igrleon@gmail.com>
2015-10-31 15:27:02 +0200

    change main page view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorization, only: :show
 
   # GET /posts
   # GET /posts.json
