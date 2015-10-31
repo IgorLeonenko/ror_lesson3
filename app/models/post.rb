5558d6983e44e8eaae2be783577f11961a96600c
Leonenko Igor <igrleon@gmail.com>
2015-10-31 17:14:19 +0200

    adding sharp sign before every tag word

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :title, :body
   validates :title, uniqueness: true, length: { in: 5..100 }
+
+  before_save :sharp_to_tag
+
+  private
+
+  def sharp_to_tag
+    self.tags = self.tags.scan(/\w+/).map{|i| '#' + i}.join(" ")
+  end
 end
