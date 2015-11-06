160cb623b491eefbd5c246044e54738a09fdbc08
Leonenko Igor <igrleon@gmail.com>
2015-11-06 19:26:54 +0200

    add modal block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authorization
+  before_action :authorization, :counter
+  helper_method :current_user
 
   protected
 
@@ -16,9 +17,14 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def counter
+    if session[:user_id].present?
+      session[:count].nil? ? session[:count] = 0 : session[:count] += 1
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
 
 end
