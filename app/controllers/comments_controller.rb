e2fe72f1e67b94c12bbe921ceb1c9703ea0c0062
Leonenko Igor <igrleon@gmail.com>
2015-11-16 16:23:37 +0200

    add comments to show page, add scrolling js plugin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,50 @@
 class CommentsController < ApplicationController
 
+  before_action :set_post
+  before_action :set_comment, only: [:update, :destroy]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.post = @post
     if @comment.save
       flash[:notice] = 'Comment was successfully created!'
-      redirect_to root_path
+      check_path
     else
-      flash[:alert] = "Cant add comment.\n#{@comment.errors.full_messages.join('')}"
-      redirect_to root_path
+      flash[:alert] = "Cant add comment.\n#{@comment.errors.full_messages.join(',')}"
+      check_path
     end
   end
 
   def update
-    @comment = Comment.find_by(user:params[:user_id], post: params[:post_id])
     if @comment.update(comment_params)
       flash[:notice] = 'Comment was successfully updated!'
-      redirect_to root_path
+      check_path
     else
-      flash[:alert] = 'Can\'t update comment.\n#{@comment.errors.full_messages.join('')}'
-      redirect_to root_path
+      flash[:alert] = 'Can\'t update comment.\n#{@comment.errors.full_messages.join(',')}'
+      check_path
     end
   end
 
   def destroy
-    @comment = Comment.find_by(user:params[:user_id], post: params[:post_id])
     @comment.destroy
-    redirect_to root_path
+    check_path
     flash[:notice] = 'Comment was successfully destroyed!'
   end
 
   private
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = Comment.find_by(user:params[:user_id], post: params[:post_id])
+    end
+
+    def check_path
+      redirect_to URI(request.referer).path == user_post_path(current_user, @post) ? :back : root_path
+    end
 
     def comment_params
       params.require(:comment).permit(:body)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @comment = Comment.new
     respond_to do |format|
       format.html { @post }
       format.json { render json: json_format(@post) }
