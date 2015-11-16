d669f102c4be2d6a62711feff449fabba3239809
Leonenko Igor <igrleon@gmail.com>
2015-11-16 22:20:46 +0200

    delete key count from session after logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
+    session.delete(:count)
     redirect_to root_path, notice: 'You are log out!'
   end
 
