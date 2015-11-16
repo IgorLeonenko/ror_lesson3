fb7b511d0f3fb7c00f5a42840d605d4e8710363c
Leonenko Igor <igrleon@gmail.com>
2015-11-16 14:11:37 +0200

    probably last trying

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
   before_save :sharp_to_tag
 
   default_scope  { order(:created_at => :desc) }
-  scope :popular, -> { unscoped.joins(:likes).group(:post_id).having("count(post_id) > 9") }
+  scope :popular, -> { unscoped.select('posts.*').joins(:likes).group('posts.id').having('count(posts.id) > 9') }
 
   def self.search(search)
     if search
