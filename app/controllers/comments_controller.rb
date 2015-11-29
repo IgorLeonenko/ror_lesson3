ef5272547a69ffa92250926d2dac2ea7066871b7
Leonenko Igor <igrleon@gmail.com>
2015-11-29 13:34:40 +0200

    ajax-ing comment CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,35 +2,21 @@ class CommentsController < ApplicationController
 
   before_action :set_post
   before_action :set_comment, only: [:update, :destroy]
-  before_action :user_check, only: [:update, :destroy]
+  before_action :user_check, only: [ :destroy]
 
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.post = @post
-    if @comment.save
-      flash[:notice] = 'Comment was successfully created!'
-      check_path
-    else
-      flash[:alert] = "Cant add comment.\n#{@comment.errors.full_messages.join(',')}"
-      check_path
-    end
+    @comment.save
   end
 
   def update
-    if @comment.update(comment_params)
-      flash[:notice] = 'Comment was successfully updated!'
-      check_path
-    else
-      flash[:alert] = 'Can\'t update comment.\n#{@comment.errors.full_messages.join(',')}'
-      check_path
-    end
+    @comment.update(comment_params)
   end
 
   def destroy
     @comment.destroy
-    check_path
-    flash[:notice] = 'Comment was successfully destroyed!'
   end
 
   private
@@ -40,11 +26,7 @@ class CommentsController < ApplicationController
     end
 
     def set_comment
-      @comment = Comment.find_by(user:params[:user_id], post: params[:post_id])
-    end
-
-    def check_path
-      redirect_to URI(request.referrer).path == user_post_path(current_user.id, @post.id) ? user_post_path(current_user.id, @post.id) : root_path
+      @comment = Comment.find_by(user:current_user.id, post: params[:post_id], id: params[:id])
     end
 
     def user_check
